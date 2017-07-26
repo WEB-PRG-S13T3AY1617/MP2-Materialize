20c15f50316c530631d73006f7812aa32433671b
Denrick Laborada <denricklaborada@users.noreply.github.com>
2017-07-26 09:05:06 +0800

    minor changes

--- mp2/marketplace/models.py
+++ mp2/marketplace/models.py
@@ -6,14 +6,14 @@ class User(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    degree_or_office = models.CharField(max_length=20)
+    degree_or_office = models.CharField(max_length=30, default='')
 
     def __str__(self):
         return self.username
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
     name = models.CharField(max_length=50)
     condition = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
@@ -22,8 +22,8 @@ class Post(models.Model):
         ('1', 'Academic'),
         ('2', 'Office'),
     )
-    academic_or_office = models.CharField(max_length=1, choices=CHOICES)
-    course = models.CharField(max_length=20, default=0)
+    academic_or_office = models.CharField(max_length=1, choices=CHOICES, default='')
+    course = models.CharField(max_length=20, default='')
 
     def __str__(self):
         return self.name
